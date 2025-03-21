b8e85849fa50d035030a10595b1d81a2ef648e42
HrushiBorhade <hrushiborhade123@gmail.com>
2025-03-21 09:04:33 +0530

    feat: Add argument handling

--- io.js
+++ io.js
@@ -3,9 +3,22 @@
 "use strict";
 
 // console.log(process.argv.slice(2));
-var args = require("minimist")(process.argv.slice(2));
+var args = require("minimist")(process.argv.slice(2),{
+    boolean:["help"],
+    string: ["file"],
+});
 console.log("arguments", args);
 
+if(args.file){
+    console.log("processing file:", args.file);
+}
+else if(args.help) {
+    printHelp();
+}
+else {
+    handleError("Incorrect usage", true);
+}
+console.log("************************************")
 console.log("Hello World!");
 
 process.stdout.write("Hello World!\n");
@@ -13,3 +26,24 @@ process.stdout.write("Hello World!\n");
 console.error("Oops");
 
 process.stderr.write("Oops\n");
+
+console.log("************************************")
+
+function handleError(errorMessage, showHelp) {
+    console.error(errorMessage);
+    if(showHelp) {
+        console.log("");
+        printHelp();
+    }
+}
+
+// *************************
+
+function printHelp() {
+    console.log("io usage:");
+    console.log("./io.js --file={FILENAME} --help");
+    console.log("");
+    console.log("--help                prints this help");
+    console.log("--file={FILENAME}     process the file");
+    console.log("");
+}
